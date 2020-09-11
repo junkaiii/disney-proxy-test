1b424ce21a10a62e066f0b24e9e72fbd3414fe0d
Kai <junkaiii@gmail.com>
2020-09-11 12:21:55 +0800

    make url dynamic

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 const port = 3000;
 
-const API_SERVICE_URL = "https://www.ipchicken.com";
+const API_SERVICE_URL = "https://jsonplaceholder.typicode.com";
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.use(
-  "/proxy",
+  "/json_placeholder",
   createProxyMiddleware({
     target: API_SERVICE_URL,
     changeOrigin: true,
     pathRewrite: {
-      [`^/proxy`]: "",
+      [`^/json_placeholder`]: "",
     },
   })
 );
